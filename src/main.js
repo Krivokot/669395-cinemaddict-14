2726e559c9dff93563cb352c2d2109757f144a13
Krivokot <scratch18693@gmail.com>
2021-04-04 22:59:37 +0300

    Добавляет компоненты и их рендер

--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
-import {  } from './view/card-info.js';
-import {  } from './view/card.js';
+import { createFilmInfoPopupTemplate } from './view/card-info.js';
+import { createFilmCardTemplate } from './view/card.js';
 import { createSiteMenuTemplate } from './view/menu.js';
-import {  } from './view/show-button.js';
-import {  } from './view/user-grade.js';
+import { createShowMoreButtonTemplate } from './view/show-button.js';
+import { createUserGradeTemplate } from './view/user-grade.js';
+import { createFilmListTemplate } from './view/card-list.js';
 
 const render = (container, template, place) => {
     container.insertAdjacentHTML(place, template)
 }
 
 const mainPageElement = document.querySelector('.main');
+const mainHeaderElement = document.querySelector('.header');
 
+render (mainHeaderElement, createUserGradeTemplate(), 'beforeend')
 render (mainPageElement, createSiteMenuTemplate(), 'beforeend')
+render (mainPageElement, createFilmListTemplate(), 'beforeend')
+// render (mainPageElement, createFilmInfoPopupTemplate(), 'beforeend')
+
+const filmsBlockElement = mainPageElement.querySelector('.films-list');
+const filmsListContainerElement = filmsBlockElement.querySelector('.films-list__container');
+
+render (filmsListContainerElement, createFilmCardTemplate(), 'beforeend')
+render (filmsBlockElement, createShowMoreButtonTemplate(), 'beforeend')
+
+
+
