72dbc217323291d0f7720f0a1837912c9cdd428a
Krivokot <37597505+Krivokot@users.noreply.github.com>
2021-04-21 21:10:07 +0300

    Правит линт

--- src/view/show-button.js
+++ src/view/show-button.js
@@ -1,7 +1,9 @@
 import AbstractView from './abstract.js';
 
 const createShowMoreButtonTemplate = () => {
-  return `<button class="films-list__show-more">Show more</button>`;
+  return `
+  <button class="films-list__show-more">Show more</button>
+  `;
 };
 
 export default class ShowMoreButton extends AbstractView {
