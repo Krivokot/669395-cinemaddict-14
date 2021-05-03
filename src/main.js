666f5c873d0888157a20439fea77284985517fa0
Krivokot <scratch18693@gmail.com>
2021-05-03 23:24:48 +0300

    Подключает презентер

--- src/main.js
+++ src/main.js
@@ -1,107 +1,29 @@
-import CardPopupView from './view/card-popup.js';
-import CardView from './view/card.js';
+import MovieListPresenter from './presenter/movie-list.js';
 import SiteMenuView from './view/menu.js';
-import ShowMoreButtonView from './view/show-button.js';
-import UserGradeView from './view/user-grade.js';
-import CardListView from './view/card-list.js';
-import EmptyListView from './view/no-card-list.js';
-import { generateCard } from './mock/card-mock.js';
-import SortMenuView from './view/sort.js';
-import { render } from './utils/render.js';
-import {isEscEvent} from './utils/common.js';
 
-const CARDS_COUNT = 25;
-const CARDS_COUNT_PER_STEP = 5;
+import UserGradeView from './view/user-grade.js';
+
+
+import { generateCard } from './mock/card-mock.js';
+
+import { render } from './utils/render.js';
+
+
+const CARDS_COUNT = 1;
 
 const cards = new Array(CARDS_COUNT).fill().map(generateCard);
 
 const mainPageElement = document.querySelector('.main');
 const mainHeaderElement = document.querySelector('.header');
-const bodyElement = document.querySelector('body');
-const cardListElement = new CardListView();
 
 render (mainHeaderElement, new UserGradeView().getElement());
 render (mainPageElement, new SiteMenuView().getElement());
-render (mainPageElement, new SortMenuView().getElement());
-render (mainPageElement, cardListElement.getElement());
-
-const filmsListElement = cardListElement.getElement().querySelector('.films-list');
-const filmsListContainerElement = filmsListElement.querySelector('.films-list__container');
-
-const renderCard = (cardListContainerElement, card) => {
-  const cardComponent = new CardView(card);
-  render(cardListContainerElement, cardComponent.getElement());
-};
-
-if (cards.length > 0) {
-  for (let i = 0; i < Math.min(cards.length, CARDS_COUNT_PER_STEP); i++) {
-    renderCard(filmsListContainerElement, cards[i]);
-  }
-} else {
-  render (mainPageElement, new EmptyListView().getElement());
-}
-
-const generatePopup = () => {
-
-  const filmCardsNode = filmsListContainerElement.querySelectorAll('.film-card');
-
-  filmCardsNode.forEach((card, index) => {
-    const cardPopupComponent = new CardPopupView(cards[index]);
-    card.addEventListener('click', () => {
-      render(mainPageElement, cardPopupComponent.getElement());
-      bodyElement.classList.add('hide-overflow');
-      closePopupByAction(cardPopupComponent);
-    });
-  });
-};
-
-const closePopup = (component) => {
-  component.getElement().remove();
-  component.removeElement();
-  bodyElement.classList.remove('hide-overflow');
-};
-
-const closePopupByAction = (component) => {
-  const closePopupButton =  component.getElement().querySelector('.film-details__close-btn');
-
-  closePopupButton.addEventListener('click', () => {
-    closePopup(component);
-  });
-
-  const closePopupByKey = (evt) => {
-    if (isEscEvent(evt)) {
-      evt.preventDefault();
-      closePopup(component);
-      document.removeEventListener('keydown', closePopupByKey);
-    }
-  };
-
-  document.addEventListener('keydown', closePopupByKey);
-};
-
-generatePopup();
-
-if (cards.length > CARDS_COUNT_PER_STEP) {
-  let renderedCardCount = CARDS_COUNT_PER_STEP;
-  const showMoreButtonComponent = new ShowMoreButtonView();
-
-  render(filmsListElement, showMoreButtonComponent.getElement());
-
-  showMoreButtonComponent.setClickHandler(() => {
-
-    cards
-      .slice(renderedCardCount, renderedCardCount + CARDS_COUNT_PER_STEP)
-      .forEach((card) => renderCard (filmsListContainerElement, card));
 
 
-    renderedCardCount += CARDS_COUNT_PER_STEP;
 
-    if (renderedCardCount >= cards.length) {
-      showMoreButtonComponent.getElement().remove();
-      showMoreButtonComponent.removeElement();
-    }
+const filmListPresenter = new MovieListPresenter(mainPageElement);
+filmListPresenter.init(cards);
 
-    generatePopup();
-  });
-}
+
+
 
--- src/presenter/movie-list.js
+++ src/presenter/movie-list.js
@@ -1,66 +1,84 @@
-import CardPopupView from './view/card-popup.js';
-import CardView from './view/card.js';
-import SiteMenuView from './view/menu.js';
-import ShowMoreButtonView from './view/show-button.js';
-import UserGradeView from './view/user-grade.js';
-import CardListView from './view/card-list.js';
-import EmptyListView from './view/no-card-list.js';
-import { generateCard } from './mock/card-mock.js';
-import SortMenuView from './view/sort.js';
-import { render } from './utils/render.js';
-import {isEscEvent} from './utils/common.js';
+import CardView from '../view/card.js';
+import ShowMoreButtonView from '../view/show-button.js';
+import CardListView from '../view/card-list.js';
+import EmptyListView from '../view/no-card-list.js';
+import SortMenuView from '../view/sort.js';
+import { render } from '../utils/render.js';
+
 
 const CARDS_COUNT_PER_STEP = 5;
 
 export default class MovieList {
-  constructor(moviesContainer) {
-    this._moviesContainer = moviesContainer;
+  constructor(main) {
+    this._main = main;
 
     this._cardComponent = new CardView();
     this._sortComponent = new SortMenuView();
     this._cardListComponent = new CardListView();
     this._noCardsComponent = new EmptyListView();
-    this._menuComponent = new SiteMenuView();
-    this._userGradeComponent = new UserGradeView();
+    this._showMoreButtonComponent = new ShowMoreButtonView();
   }
 
-  init(movieListCards) {
-    this._movieListCards = movieListCards.slice();
-
-    render (mainPageElement, this._cardListComponent);
-
-    this._renderMovieList();
+  init(card) {
+  
+    this._renderMovieList(card);
   }
 
   _renderSort() {
-    render(mainPageElement, this._sortComponent);
+    render(this._main, this._sortComponent.getElement());
   }
 
   _renderCard(card) {
-
-
+    console.log(card);
+    const filmsListElement = this._cardListComponent.getElement().querySelector('.films-list');
+    const filmsListContainerElement = filmsListElement.querySelector('.films-list__container');
+    for (let i = 0; i > card.length; i++) {
+      render(filmsListContainerElement, this._cardComponent(card[i]).getElement());
+    }
+    
   }
 
   _renderCardList() {
-    render(mainPageElement, this._cardListComponent);
+    render(this._main, this._cardListComponent.getElement());
   }
 
   _renderNoCards() {
-
+    render(this._main, this._noCardsComponent.getElement());
   }
 
-  _renderShowMoreButton() {
+  _handleShowMoreButtonClick(cards) {
+  let renderedCardCount = CARDS_COUNT_PER_STEP;
 
+    cards
+    .slice(renderedCardCount, renderedCardCount + CARDS_COUNT_PER_STEP)
+    .forEach((card) => this._renderCard);
+
+
+    renderedCardCount += CARDS_COUNT_PER_STEP;
+
+    if (renderedCardCount >= cards.length) {
+      this._showMoreButtonComponent.getElement().remove();
+      this._showMoreButtonComponent.removeElement();
+    } 
   }
 
-  _renderMovieList() {
-    if (this._movieListCards.length > 0) {
-      for (let i = 0; i < Math.min(this._movieListCards.length, CARDS_COUNT_PER_STEP); i++) {
-        renderCard(filmsListContainerElement, this._movieListCards[i]);
-      }
+  _renderShowMoreButton(card) {
+    const filmsListElement = this._cardListComponent.getElement().querySelector('.films-list');
+
+    render(filmsListElement, this._showMoreButtonComponent.getElement());
+
+    this._showMoreButtonComponent.setClickHandler(this._handleShowMoreButtonClick(card));
+  }
+
+  _renderMovieList(card) {
+    this._renderSort();
+    this._renderCardList();
+    this._renderShowMoreButton(card);
+
+    if (card.length > 0) {
+      this._renderCard(card);
+    } else {
+      this._renderNoCards();
     }
-
-    this._renderSort;
-    this._renderCardList;
   }
 }
